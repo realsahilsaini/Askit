6c71e1933b74f522882531cfa4d73bec98932544
Sahil Saini <thisissahilsaini@gmail.com>
2022-10-12 20:46:52 +0530

    client & server connection established

--- Backend/db.js
+++ Backend/db.js
@@ -1,1 +1,18 @@
 //Database connection 
+
+const mongoose = require('mongoose')
+
+const url  = "mongodb+srv://test:9GSnPmDkympy0UOE@cluster0.spdkgen.mongodb.net/?retryWrites=true&w=majority";
+
+module.exports.connect = () => {
+    mongoose
+      .connect(url, {
+        //for avoiding unexpected
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("MongoDB connected successfully");
+      })
+      .catch((error) => console.log("Error: ", error));
+  };
--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,15 @@
 //Server's entry point (Handshaking of frontend and backend)
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 const app = express()
-const bodyParser = require('body-parer')
+const bodyParser = require('body-parser');
 const PORT = 80;
+const db = require('./db');
+const router = require("./routes");
+
+//Database Connection
+db.connect();
 
 //middle ware
 app.use(bodyParser.json({limit: "50mb"}))
@@ -12,25 +17,26 @@ app.use(bodyParser.urlencoded({extended: true, limit: "50mb"}))
 
 //cors
 app.use((req, res, next) => {
-    req.headers("Access-Control-Allow-Origin", "*")
-    req.headers("Access-Control-Allow-Headers", "*");
+    req.header("Access-Control-Allow-Origin", "*")
+    req.header("Access-Control-Allow-Headers", "*");
     next()
 })
 
 //routes
 
+app.use('/api', router)
 
 
 
 
-app.use('/uploads', express.static(path.join(__dirname, "/../uploads")))
-app.use('/uploads', express.static(path.join(__dirname, "/../Frontend/build")));
+app.use('/uploads', express.static(path.join(__dirname, "/../uploads")));
+app.use(express.static(path.join(__dirname, '/../frontend/build')));
 
 app.get("*", (req,res)=> {
     try{
-        res.sendFile(path.join(`${__dirname}/../Frontend/build/index.html`))
+        res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
     } catch(e){
-        res.send("Oops! unexpected error")
+        res.send("Oops! unexpected error");
     }
 })
 
